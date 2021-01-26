7b670a82d9bfacc08184a2c0f3e808977cae2968
FreeDream <2511200510@qq.com>
2021-01-26 10:19:25 +0800

    加入提取ini的函数

--- controller/controllers.go
+++ controller/controllers.go
@@ -1,3 +1,12 @@
-package healing2020
+package controller
 
+import (
+    "healing2020/pkg/tools"
+	"github.com/gin-gonic/gin"
+)
+
+func Test(c *gin.Context) {
+    value := tools.GetConfig("test","appid")
+    c.JSON(200,gin.H{"test":value})
+}
 
