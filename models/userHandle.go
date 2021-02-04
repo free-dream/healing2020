0210bdd4ac475c657ef4be1c8013273d12f03f95
FreeDream <2511200510@qq.com>
2021-02-04 23:47:59 +0800

    模型部分更新

--- models/userHandle.go
+++ models/userHandle.go
@@ -6,20 +6,22 @@ import (
     
     "github.com/jinzhu/gorm"
     _ "github.com/jinzhu/gorm/dialects/mysql"
+
+    "errors"
 )
 
 func UpdateOrCreate(openId string,nickName string) {
     db := setting.MysqlConn()
     db.Transaction(func(tx *gorm.DB) error {
         var user statements.User
-        result := tx.Model(&statements.User{}).Where("open_id=?",openid).First(&user)
+        result := tx.Model(&statements.User{}).Where("open_id=?",openId).First(&user)
         user.NickName = nickName
         user.OpenId = openId
         var result2 *gorm.DB
         if errors.Is(result.Error, gorm.ErrRecordNotFound) {
-            result2 := tx.Model(&statements.User{}).Create(&user)
+            result2 = tx.Model(&statements.User{}).Create(&user)
         }else {
-            result2 := tx.Model(&statements.User{}).Where("open_id=?",openId).Update(&user)
+            result2 = tx.Model(&statements.User{}).Where("open_id=?",openId).Update(&user)
         }
         return result2.Error
     })
