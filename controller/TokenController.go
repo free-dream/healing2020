1b3e355ae866746e82223cc4e2ab6d9d4dd5b5ae
hanbest <1506207477@qq.com>
2021-02-04 23:30:43 +0800

    修改七牛token接口undefined问题

--- controller/TokenController.go
+++ controller/TokenController.go
@@ -1,9 +1,6 @@
 package controller
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/qiniu/api.v7/v7/auth/qbox"
 	"github.com/qiniu/api.v7/v7/storage"
 	"github.com/gin-gonic/gin"
@@ -17,7 +14,7 @@ import (
 //@Router /qiniu/token [get]
 //@Success 200 {string} string "{"upToken": "xxxxx"}"
 //@Failure 403 {string} string "{"err": "false"}"
-func qiniuToken(c *gin.Context) {
+func QiniuToken(c *gin.Context) {
   	//设置七牛基本信息
 	accessKey := tools.GetConfig("qiniu", "accessKey")
 	secretKey := tools.GetConfig("qiniu", "secretKey")
@@ -30,7 +27,7 @@ func qiniuToken(c *gin.Context) {
 	upToken := putPolicy.UploadToken(mac)
 	err := "false"
 	//返回toekn
-	if upToken != nil {
+	if(upToken != "") {
 		c.JSON(200, upToken)
 	}else{
 		c.JSON(403, err)
