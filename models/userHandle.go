d055ff3fc03afa7d512db9a3b97497fb1f39046d
FreeDream <2511200510@qq.com>
2021-02-05 23:06:05 +0800

    修改userHandle

--- models/userHandle.go
+++ models/userHandle.go
@@ -14,7 +14,7 @@ func UpdateOrCreate(openId string,nickName string) {
     db := setting.MysqlConn()
     db.Transaction(func(tx *gorm.DB) error {
         var user statements.User
-        result := tx.Model(&statements.User{}).Where("open_id=?",openId).First(&user)
+        result := tx.Model(&statements.User{}).Where("open_id=?",openid).First(&user)
         user.NickName = nickName
         user.OpenId = openId
         var result2 *gorm.DB
