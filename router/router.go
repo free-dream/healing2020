fae13868e69b4bdbe56ab926c618f348017ba4e6
FreeDream <2511200510@qq.com>
2021-03-04 17:42:40 +0800

    添加api分组

--- router/router.go
+++ router/router.go
@@ -15,46 +15,47 @@ func InitRouter() *gin.Engine {
     r.Use(gin.Recovery())
 
     //开发时按群组分类，并记得按swagger格式注释
+    api := r.Group("/api")
     
     //qiniuToken
-    r.GET("/qiniu/token", controller.QiniuToken)
+    api.GET("/qiniu/token", controller.QiniuToken)
 
     //注册
-    r.POST("/register", controller.Register)
+    api.POST("/register", controller.Register)
 
     //添加爱好
-    r.POST("/user/hobby", controller.NewHobby)
+    api.POST("/user/hobby", controller.NewHobby)
     //获取爱好
-    r.GET("/user/hobby", controller.GetHobby)
+    api.GET("/user/hobby", controller.GetHobby)
 
     //修改个人信息
     r.PUT("/user", controller.PutUser)
     //个人页
-    r.GET("/user", controller.ResponseMyPerponalPage)
+    api.GET("/user", controller.ResponseMyPerponalPage)
 
     //修改用户个人背景
-    r.POST("/user/background", controller.ChangeBackground)
+    api.POST("/user/background", controller.ChangeBackground)
 
     //rank
-    r.GET("/deliver/rank",controller.DeliverRank)
-    r.GET("/songs/rank",controller.SongRank)
-    r.GET("/user/rank",controller.UserRank)
+    api.GET("/deliver/rank",controller.DeliverRank)
+    api.GET("/songs/rank",controller.SongRank)
+    api.GET("/user/rank",controller.UserRank)
 
     //main
-    r.GET("/main/page",controller.MainMsg)
+    api.GET("/main/page",controller.MainMsg)
 
     //heal
-    r.GET("/user/phone",controller.PhoneHeal)
-    r.GET("/record",controller.Record)
-    r.GET("/like",controller.Praise)
-    r.POST("/record",controller.RecordHeal)
-    r.POST("/vod",controller.VodPost)
+    api.GET("/user/phone",controller.PhoneHeal)
+    api.GET("/record",controller.Record)
+    api.GET("/like",controller.Praise)
+    api.POST("/record",controller.RecordHeal)
+    api.POST("/vod",controller.VodPost)
 
     //swagger
-    r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+    api.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
     //test
-    r.GET("/initest",controller.Test)
+    api.GET("/initest",controller.Test)
 
     //god view
 
