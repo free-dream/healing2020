bed0eabd5c9f6d1448ecd7d2946c251601b6e401
forseason <ysy604822@live.com>
2021-04-26 20:06:23 +0800

    删除controller/singSubjectController.go的错误处理

--- controller/singSubjectController.go
+++ controller/singSubjectController.go
@@ -2,16 +2,11 @@ package controller
 
 import (
 	"healing2020/models"
-	"healing2020/pkg/e"
 
 	"github.com/gin-gonic/gin"
 )
 
 func SingSubject(c *gin.Context) {
-	sub, err := models.SingSubject()
-	if err != nil {
-		c.JSON(403, e.ErrMsgResponse{Message: e.GetMsg(e.INVALID_PARAMS)})
-		return
-	}
+	sub, _ := models.SingSubject()
 	c.JSON(200, sub)
 }
