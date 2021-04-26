67a397f440c6a7edf37a0efa07ae47b0d0297867
forseason <ysy604822@live.com>
2021-04-26 22:34:38 +0800

    修复identityCheck中间件startwith方法逻辑错误

--- controller/middleware/identityCheck.go
+++ controller/middleware/identityCheck.go
@@ -1,40 +1,43 @@
 package middleware
 
 import (
-    "github.com/gin-gonic/gin"
-    "github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-gonic/gin"
 
-    "healing2020/pkg/tools"
-    "healing2020/pkg/e"
+	"healing2020/pkg/e"
+	"healing2020/pkg/tools"
 )
 
-func IdentityCheck() gin.HandlerFunc{
-    return func(c *gin.Context) {
-        rUrl := c.Request.URL.Path
-        session := sessions.Default(c)
-        token := session.Get("token")
+func IdentityCheck() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		rUrl := c.Request.URL.Path
+		session := sessions.Default(c)
+		token := session.Get("token")
 
-        if startWith(rUrl,"/auth") {
-            c.Next()
-        }
-        if tools.IsZeroValue(token) {
-            if startWith(rUrl,"/api") {
-                c.JSON(401,e.ErrMsgResponse{Message:"fail to authenticate"})
-                c.Abort()
-                return
-            }else {
-                redirect := c.Query("redirect")
-                url := "https://healing2020.100steps.top/auth/jump"+redirect
-                c.Redirect(302,url)
-                c.Abort()
-                return
-            }
-        }
-        c.Next()
-    }
+		if startWith(rUrl, "/auth") {
+			c.Next()
+		}
+		if tools.IsZeroValue(token) {
+			if startWith(rUrl, "/api") {
+				c.JSON(401, e.ErrMsgResponse{Message: "fail to authenticate"})
+				c.Abort()
+				return
+			} else {
+				redirect := c.Query("redirect")
+				url := "https://healing2020.100steps.top/auth/jump?redirect=" + redirect
+				c.Redirect(302, url)
+				c.Abort()
+				return
+			}
+		}
+		c.Next()
+	}
 }
 
-func startWith(rUrl string,uri string) bool{
-    rUrl = rUrl[0:len(uri)-1]
-    return rUrl == uri
+func startWith(rUrl string, uri string) bool {
+	if len(uri) > len(rUrl) {
+		return false
+	}
+	rUrl = rUrl[0:len(uri)]
+	return rUrl == uri
 }
