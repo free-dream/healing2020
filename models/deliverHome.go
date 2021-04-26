6397072fcd4b840eeaece2a21d446e5e35d4e3d7
SUSLIK <395268471@qq.com>
2021-04-26 16:37:48 +0800

    增加投递首页用户昵称

--- models/deliverHome.go
+++ models/deliverHome.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"healing2020/models/statements"
 	"healing2020/pkg/setting"
 
 	_ "github.com/jinzhu/gorm/dialects/mysql"
@@ -15,7 +16,11 @@ type User struct {
 	Praise    int    `json:"praise"`
 }
 
-func DeliverHome() ([]User, error) {
+type AllDeliver struct {
+	Deliverelse User
+	Nickname  string    `json:"nickname"`
+}
+func DeliverHome() ([]AllDeliver, error) {
 	//连接mysql
 	db := setting.MysqlConn()
 	defer db.Close()
@@ -23,5 +28,23 @@ func DeliverHome() ([]User, error) {
 	//获取投递信息
 	var deliverHome []User
 	err := db.Table("deliver").Select("user_id, type, text_field, photo, record, praise").Scan(&deliverHome).Error
-	return deliverHome, err
+
+	//获取用户昵称
+	UserNickname := make([]statements.User, len(deliverHome))
+	for i := 0; i < len(deliverHome); i++ {
+		err = db.Table("User").Select("nick_name").Where("id = ?", deliverHome[i].UserID).Scan(&UserNickname[i]).Error
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	responseDeliver := make([]AllDeliver, len(deliverHome))
+	for i := 0; i < len(deliverHome); i++ {
+		responseDeliver[i] = AllDeliver{
+			Deliverelse:    deliverHome[i],
+			Nickname:  		UserNickname[i].NickName,
+		}
+	}
+	return responseDeliver, err
+
 }
