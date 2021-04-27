3d7371e958ccd8cf1f62ff12dc5f5cf474226d33
forseason <ysy604822@live.com>
2021-04-27 12:54:16 +0800

    将数据库连接编码改成utf8mb4

--- pkg/setting/mysqlConn.go
+++ pkg/setting/mysqlConn.go
@@ -1,37 +1,37 @@
 package setting
 
 import (
-    "healing2020/pkg/tools"
-    "github.com/jinzhu/gorm"
-    _ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"healing2020/pkg/tools"
 )
 
 func MysqlConnTest() {
-    dbName := tools.GetConfig("mysql","dbName")
-    user := tools.GetConfig("mysql","user")
-    password := tools.GetConfig("mysql","password")
-    port := tools.GetConfig("mysql","port")
-    dbInfo := user+":"+password+"@tcp("+port+")/"+dbName+"?charset=utf8&parseTime=True&loc=Local"
+	dbName := tools.GetConfig("mysql", "dbName")
+	user := tools.GetConfig("mysql", "user")
+	password := tools.GetConfig("mysql", "password")
+	port := tools.GetConfig("mysql", "port")
+	dbInfo := user + ":" + password + "@tcp(" + port + ")/" + dbName + "?charset=utf8mb4&parseTime=True&loc=Local"
 
-    //connect
-    db,err := gorm.Open("mysql",dbInfo)
-    db.SingularTable(true)
-    db.Close()
-    if err != nil {
-        panic(err)
-    }
+	//connect
+	db, err := gorm.Open("mysql", dbInfo)
+	db.SingularTable(true)
+	db.Close()
+	if err != nil {
+		panic(err)
+	}
 }
 
-func MysqlConn() *gorm.DB{
-    dbName := tools.GetConfig("mysql","dbName")
-    user := tools.GetConfig("mysql","user")
-    password := tools.GetConfig("mysql","password")
-    port := tools.GetConfig("mysql","port")
-    dbInfo := user+":"+password+"@tcp("+port+")/"+dbName+"?charset=utf8&parseTime=True&loc=Local"
+func MysqlConn() *gorm.DB {
+	dbName := tools.GetConfig("mysql", "dbName")
+	user := tools.GetConfig("mysql", "user")
+	password := tools.GetConfig("mysql", "password")
+	port := tools.GetConfig("mysql", "port")
+	dbInfo := user + ":" + password + "@tcp(" + port + ")/" + dbName + "?charset=utf8&parseTime=True&loc=Local"
 
-    //connect
-    db,_ := gorm.Open("mysql",dbInfo)
-    db.SingularTable(true)
+	//connect
+	db, _ := gorm.Open("mysql", dbInfo)
+	db.SingularTable(true)
 
-    return db
+	return db
 }
