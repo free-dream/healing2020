f683c89063895526fdeb51695f9ce62924a6c7b8
SUSLIK <395268471@qq.com>
2021-04-30 18:16:24 +0800

    修改歌房详情页返回内容

--- models/singHome.go
+++ models/singHome.go
@@ -3,6 +3,7 @@ package models
 import (
 	"healing2020/models/statements"
 	"healing2020/pkg/setting"
+	"time"
 
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
@@ -11,16 +12,29 @@ type AllSpecial struct {
 	ID       uint   `json:"id"`
 	Name     string `json:"name"`
 	Intro    string `json:"intro"`
-	SingSong []Sing
+	Photo	 string `json:"photo"`
+	SingSong []UserMessage
 }
 
 type Sing struct {
-	UserID uint   `json:"id"`
-	Name   string `json:"name"`
-	Praise string `json:"praise"`
-	Song   string `json:"song"`
+	UserID		uint   		`json:"user_id"`
+	Id 			int			`json:"id"`
+	CreatedAt	time.Time 	`json:"createdat"`
+	Praise 		string 		`json:"praise"`
+	Song   		string 		`json:"name"`
 }
 
+type UserMessage struct {
+	Nickname	string `json:"user"`
+	Avatar		string `json:"avatar"`
+	SingElse	Sing
+	
+	//无用数据
+	Type 		string `json:"type"`
+	Photo 		string `json:"photo"`
+	Text		string `json:"text"`
+	Source		string `json:"source"`
+}
 func SingHome(subjectID uint) (AllSpecial, error) {
 	//连接mysql
 	db := setting.MysqlConn()
@@ -28,16 +42,30 @@ func SingHome(subjectID uint) (AllSpecial, error) {
 
 	//获取歌房信息
 	var singSubject statements.Subject
-	err := db.Table("subject").Select("id, name, intro").Where("id = ? ", subjectID).First(&singSubject).Error
+	err := db.Table("subject").Select("id, name, intro, photo").Where("id = ? ", subjectID).First(&singSubject).Error
 
 	var SingHome []Sing
-	err = db.Table("special").Select("user_id, name, praise, song").Where("subject_id = ? ", subjectID).Scan(&SingHome).Error
+	err = db.Table("special").Select("user_id, id, created_at, praise, song").Where("subject_id = ? ", subjectID).Scan(&SingHome).Error
+
+	UserElse := make([]statements.User, len(SingHome))
+	for i := 0; i < len(SingHome); i++ {
+		err = db.Table("user").Select("nick_name, avatar").Where("id = ?", SingHome[i].UserID).Scan(&UserElse[i]).Error
+	}
+	
+	responseSing := make([]UserMessage, len(SingHome))
+	for i := 0; i < len(SingHome); i++ {
+		responseSing[i] = UserMessage{
+			Nickname:   UserElse[i].NickName,
+			Avatar:     UserElse[i].Avatar,
+			SingElse: 	SingHome[i],
+		}
+	}
 
 	allSpecial := AllSpecial{
 		ID:       singSubject.ID,
 		Name:     singSubject.Name,
 		Intro:    singSubject.Intro,
-		SingSong: SingHome,
+		SingSong: responseSing,
 	}
 
 	return allSpecial, err
