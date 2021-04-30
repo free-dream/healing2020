d739e16ad9dc6df1befa47cb16ebd8f27a0b5153
SUSLIK <395268471@qq.com>
2021-04-30 21:06:54 +0800

    修改歌房歌曲返回格式，增加当前歌房评论数

--- models/singHome.go
+++ models/singHome.go
@@ -9,32 +9,30 @@ import (
 )
 
 type AllSpecial struct {
-	ID       uint   `json:"id"`
-	Name     string `json:"name"`
-	Intro    string `json:"intro"`
-	Photo	 string `json:"photo"`
-	SingSong []UserMessage
-}
-
-type Sing struct {
-	UserID		uint   		`json:"user_id"`
-	Id 			int			`json:"id"`
-	CreatedAt	time.Time 	`json:"createdat"`
-	Praise 		string 		`json:"praise"`
-	Song   		string 		`json:"name"`
+	ID        uint   `json:"id"`
+	Name      string `json:"name"`
+	Intro     string `json:"intro"`
+	MesNumber int    `json:"mesNumber"`
+	SingSong  []UserMessage
 }
 
 type UserMessage struct {
-	Nickname	string `json:"user"`
-	Avatar		string `json:"avatar"`
-	SingElse	Sing
-	
+	UserID   uint   `json:"user_id"`
+	Nickname string `json:"user"`
+	Avatar   string `json:"avatar"`
+
+	Id        int       `json:"song_id"`
+	CreatedAt time.Time `json:"time"`
+	Praise    string    `json:"praise"`
+	Song      string    `json:"name"`
+
 	//无用数据
-	Type 		string `json:"type"`
-	Photo 		string `json:"photo"`
-	Text		string `json:"text"`
-	Source		string `json:"source"`
+	Type   string `json:"type"`
+	Photo  string `json:"photo"`
+	Text   string `json:"text"`
+	Source string `json:"source"`
 }
+
 func SingHome(subjectID uint) (AllSpecial, error) {
 	//连接mysql
 	db := setting.MysqlConn()
@@ -43,29 +41,36 @@ func SingHome(subjectID uint) (AllSpecial, error) {
 	//获取歌房信息
 	var singSubject statements.Subject
 	err := db.Table("subject").Select("id, name, intro, photo").Where("id = ? ", subjectID).First(&singSubject).Error
-
-	var SingHome []Sing
+	var count int
+	err = db.Table("comment").Where("type = 1 and song_id = ?", subjectID).Count(&count).Error
+	//获取歌曲信息
+	var SingHome []UserMessage
 	err = db.Table("special").Select("user_id, id, created_at, praise, song").Where("subject_id = ? ", subjectID).Scan(&SingHome).Error
-
+	//获取用户信息
 	UserElse := make([]statements.User, len(SingHome))
 	for i := 0; i < len(SingHome); i++ {
 		err = db.Table("user").Select("nick_name, avatar").Where("id = ?", SingHome[i].UserID).Scan(&UserElse[i]).Error
 	}
-	
+
 	responseSing := make([]UserMessage, len(SingHome))
 	for i := 0; i < len(SingHome); i++ {
 		responseSing[i] = UserMessage{
-			Nickname:   UserElse[i].NickName,
-			Avatar:     UserElse[i].Avatar,
-			SingElse: 	SingHome[i],
+			Nickname:  UserElse[i].NickName,
+			Avatar:    UserElse[i].Avatar,
+			UserID:    SingHome[i].UserID,
+			Id:        SingHome[i].Id,
+			CreatedAt: SingHome[i].CreatedAt,
+			Praise:    SingHome[i].Praise,
+			Song:      SingHome[i].Song,
 		}
 	}
 
 	allSpecial := AllSpecial{
-		ID:       singSubject.ID,
-		Name:     singSubject.Name,
-		Intro:    singSubject.Intro,
-		SingSong: responseSing,
+		ID:        singSubject.ID,
+		Name:      singSubject.Name,
+		Intro:     singSubject.Intro,
+		MesNumber: count,
+		SingSong:  responseSing,
 	}
 
 	return allSpecial, err
