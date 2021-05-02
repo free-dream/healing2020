42e9499656d86782e0945e6112a9823c6237bc99
hanbest <1506207477@qq.com>
2021-05-02 22:32:55 +0800

    补充ws数据库操作的model

--- models/messageHandle.go
+++ models/messageHandle.go
@@ -1,21 +1,36 @@
 package models
 
 import (
-	"fmt"
 	"healing2020/models/statements"
 	"healing2020/pkg/setting"
+	"log"
 )
 
 //保存用户聊天的消息
-func CreateMessage(msg statements.Message) error {
+func SaveMessage(msg statements.Message) error {
 	db := setting.MysqlConn()
 	defer db.Close()
 
 	tx := db.Begin()
-	err := tx.Model(&statements.Message{}).Create(msg).Error
+	err := tx.Model(&statements.Message{}).Create(&msg).Error
 	if err != nil {
 		tx.Rollback()
-		fmt.Println(err)
+		log.Println(err)
+		return err
+	}
+	return tx.Commit().Error
+}
+
+//删除用户聊天消息
+func DeleteMessage(msg statements.Message) error {
+	db := setting.MysqlConn()
+	defer db.Close()
+
+	tx := db.Begin()
+	err := tx.Model(&statements.Message{}).Where("msg_id=?", msg.MsgID).Delete(&statements.Message{}).Error
+	if err != nil {
+		tx.Rollback()
+		log.Println(err)
 		return err
 	}
 	return tx.Commit().Error
