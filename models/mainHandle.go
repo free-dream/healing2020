808fa3f4c508cc7f45a2d6942388d5dc1fd8af59
FreeDream <2511200510@qq.com>
2021-05-11 21:24:47 +0800

    修改数据名字

--- models/mainHandle.go
+++ models/mainHandle.go
@@ -353,7 +353,7 @@ type VodResp struct {
 	VodId   uint      `json:"vodid"`
 	VodName string    `json:"name"`
 	VodUser string    `json:"user"`
-    VodUserId uint    `json:"vodUserId"`
+    VodUserId uint    `json:"userid"`
 	Avatar  string    `json:"avatar"`
 	More    string    `json:"more"`
 	Sex     int       `json:"sex"`
